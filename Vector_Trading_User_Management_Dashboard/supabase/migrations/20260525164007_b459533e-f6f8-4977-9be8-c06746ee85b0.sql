a528cd7c6c4e4077246b01b3e5dc94cb7acdfc44
Michail Koutroumpousis <mickout56@yahoo.com>
2026-05-26 18:00:03 +0300

    edit: Updating new version of Vector app

--- Vector_Trading_User_Management_Dashboard/supabase/migrations/20260525164007_b459533e-f6f8-4977-9be8-c06746ee85b0.sql
+++
@@ -1,6 +0,0 @@
-
-ALTER FUNCTION public.set_updated_at() SET search_path = public;
-ALTER FUNCTION public.set_updated_at() SECURITY INVOKER;
-
-REVOKE EXECUTE ON FUNCTION public.has_role(UUID, app_role) FROM PUBLIC, anon;
-REVOKE EXECUTE ON FUNCTION public.handle_new_user() FROM PUBLIC, anon, authenticated;
